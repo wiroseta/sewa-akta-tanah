5a0c13a7cc1e49c680b5ff8192debe75ef29a0c5
wiroseta <wiroseta@gmail.com>
2026-09-25 15:34:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Isi dari Supabase Dashboard > Project Settings > API.
 // Publishable/anon key AMAN berada di frontend bila RLS database diaktifkan.
 window.SEWA_CONFIG = {
-  supabaseUrl: 'https://yimkpqyuxntutrocmdbn.supabase.co/rest/v1/',
+  supabaseUrl: 'https://yimkpqyuxntutrocmdbn.supabase.co',
   supabaseKey: 'sb_publishable_L40RH0TrgpZ-Qj31d6Vs6A_79JIjvxv'
 };
