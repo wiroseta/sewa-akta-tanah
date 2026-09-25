1be8afb77ab7664d493b148162063c2248d2a6d5
wiroseta <wiroseta@gmail.com>
2026-09-25 15:26:02 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // Publishable/anon key AMAN berada di frontend bila RLS database diaktifkan.
 window.SEWA_CONFIG = {
   supabaseUrl: 'https://yimkpqyuxntutrocmdbn.supabase.co/rest/v1/',
-  supabaseKey: 'https://yimkpqyuxntutrocmdbn.supabase.co/rest/v1/'
+  supabaseKey: 'sb_publishable_L40RH0TrgpZ-Qj31d6Vs6A_79JIjvxv'
 };
